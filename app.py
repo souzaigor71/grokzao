3a41767c44dd4450dc4ec5856845a1a73315370e
Underground <140662084+souzaigor71@users.noreply.github.com>
2026-07-11 11:51:21 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import send_from_directory
+
+@app.route('/.well-known/assetlinks.json')
+def assetlinks():
+    return send_from_directory('static/.well-known', 'assetlinks.json', mimetype='application/json')
